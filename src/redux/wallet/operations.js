a58db85d822324f2c0202235550e10305df12569
Ihor Leonov <ihorleonow@gmail.com>
2023-07-27 03:05:53 +0300

    Update operations.js

--- src/redux/wallet/operations.js
+++ src/redux/wallet/operations.js
@@ -1,95 +1,40 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { ethers } from "ethers";
 
-// function handleEthereum() {
-//   const { ethereum } = window;
-//   if (ethereum && ethereum.isMetaMask) {
-//     console.log("Ethereum successfully detected!");
-//     // Access the decentralized web!
-//   } else {
-//     console.log("Please install MetaMask!");
-//   }
-// }
-
 export const connect = createAsyncThunk(
   "wallet/connect",
   async (_, thunkAPI) => {
-    if (window.ethereum) {
+    try {
       const { ethereum } = window;
       if (ethereum && ethereum.isMetaMask) {
         console.log("Ethereum successfully detected!");
         // Access the decentralized web!
-        try {
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-          const account = await window.ethereum.request({
-            method: "eth_requestAccounts",
-          });
-
-          const userBalance = await window.ethereum.request({
-            method: "eth_getBalance",
-            params: [account[0], "latest"],
-          });
-
-          const userChain = await provider.getNetwork();
-
-          const data = {
-            userAccount: account[0],
-            userBalance,
-            userChain: userChain.name,
-          };
-
-          return data;
-        } catch (error) {
-          console.log("Error:", error.message);
-          return thunkAPI.rejectWithValue(error.message);
-        }
       } else {
         console.log("Please install MetaMask!");
       }
-    } else {
-      window.addEventListener(
-        "ethereum#initialized",
-        async () => {
-          const { ethereum } = window;
-          if (ethereum && ethereum.isMetaMask) {
-            console.log("Ethereum successfully detected!");
-            // Access the decentralized web!
-            try {
-              const provider = new ethers.providers.Web3Provider(
-                window.ethereum
-              );
 
-              const account = await window.ethereum.request({
-                method: "eth_requestAccounts",
-              });
+      const account = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
 
-              const userBalance = await window.ethereum.request({
-                method: "eth_getBalance",
-                params: [account[0], "latest"],
-              });
+      const userBalance = await window.ethereum.request({
+        method: "eth_getBalance",
+        params: [account[0], "latest"],
+      });
 
-              const userChain = await provider.getNetwork();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const userChain = await provider.getNetwork();
 
-              const data = {
-                userAccount: account[0],
-                userBalance,
-                userChain: userChain.name,
-              };
+      const data = {
+        userAccount: account[0],
+        userBalance,
+        userChain: userChain.name,
+      };
 
-              return data;
-            } catch (error) {
-              console.log("Error:", error.message);
-              return thunkAPI.rejectWithValue(error.message);
-            }
-          }
-        },
-        {
-          once: true,
-        }
-      );
-
-      // setTimeout(handleEthereum, 3000); // 3 seconds
+      return data;
+    } catch (error) {
+      console.log("Error:", error.message);
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
