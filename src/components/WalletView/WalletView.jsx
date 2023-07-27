9527f30d9cadf776227e3c8d15b004e8c71df5e6
Ihor Leonov <ihorleonow@gmail.com>
2023-07-27 12:45:21 +0300

    add connecting in mobile

--- src/components/WalletView/WalletView.jsx
+++ src/components/WalletView/WalletView.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { ethers } from "ethers";
 import { useDispatch, useSelector } from "react-redux";
 import { connect } from "../../redux/wallet/operations";
@@ -7,17 +7,12 @@ import { ConnectBtn, AccentData, BoldAccent } from "./WalletView.styled";
 
 export const WalletView = () => {
   const dispatch = useDispatch();
-  const [visitedLink, setVisitedLink] = useState(false);
 
   const { userBalance, userAccount, userChain } = useSelector(selectUser);
 
-  // const touchDevice = "ontouchstart" in document.documentElement;
-  // console.log(touchDevice);
-
-  function isMobileDevice() {
+  const isMobileDevice = () => {
     return "ontouchstart" in window || "onmsgesturechange" in window;
-  }
-  // console.log(isMobileDevice);
+  };
 
   const formattedBalance = userBalance
     ? Number(ethers.utils.formatEther(userBalance)).toFixed(3)
@@ -46,22 +41,12 @@ export const WalletView = () => {
   // };
 
   const handleConnect = () => {
-    if (isMobileDevice() && !window.ethereum) {
+    if (isMobileDevice && !window.ethereum) {
       window.open(deepLinkURL);
-    } else {
-      // window.open("https://metamask.io/", "_blank");
-      dispatch(connect());
+    } else if (!window.ethereum && !isMobileDevice) {
+      alert("Install Metamask!");
     }
-
-    // if (touchDevice && !visitedLink) {
-    //   handleMobileConnect();
-    //   setVisitedLink(true);
-    // }
-    // if (!window.ethereum) {
-    //   alert("Install Metamask!");
-    //   return;
-    // }
-    // dispatch(connect());
+    dispatch(connect());
   };
 
   useEffect(() => {
@@ -72,24 +57,19 @@ export const WalletView = () => {
   }, []);
 
   return (
-    <>
-      <a href="https://metamask.app.link/dapp/crypto-wallet-app-ihorleonov.vercel.app/">
-        metamask.app
-      </a>
-      <ConnectBtn onClick={handleConnect}>
-        {userAccount ? (
-          <>
-            <BoldAccent>{formattedChain()}</BoldAccent>
-            &nbsp;Balance:&nbsp;
-            <AccentData>{formattedBalance}</AccentData>
-            <BoldAccent>&nbsp;ETH</BoldAccent>
-            &nbsp;Account:&nbsp;
-            <AccentData>{formattedAccount}</AccentData>
-          </>
-        ) : (
-          "Connect Wallet"
-        )}
-      </ConnectBtn>
-    </>
+    <ConnectBtn onClick={handleConnect}>
+      {userAccount ? (
+        <>
+          <BoldAccent>{formattedChain()}</BoldAccent>
+          &nbsp;Balance:&nbsp;
+          <AccentData>{formattedBalance}</AccentData>
+          <BoldAccent>&nbsp;ETH</BoldAccent>
+          &nbsp;Account:&nbsp;
+          <AccentData>{formattedAccount}</AccentData>
+        </>
+      ) : (
+        "Connect Wallet"
+      )}
+    </ConnectBtn>
   );
 };
